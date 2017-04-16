3a70907bafc4029183d6ffc43b64828dab43ddc0
Andrew Khazanovich <akhaz@assembly.cs.rutgers.edu>
2017-04-16 15:37:17 -0400

    Added bunch of notes on inode and superblock structure to the stdefs.h need to decide on implementation

--- stdefs.h
+++ stdefs.h
@@ -14,6 +14,8 @@
 // Structure Definitions
 /****************************************************************/
 
+//https://web.cs.wpi.edu/~rek/DCS/D04/UnixFileSystems.html
+// Where im getting most of information
 
 /* Inode Basics
  * An Inode number points to an Inode. An Inode is a data structure that stores the following information about a file :
@@ -27,6 +29,13 @@
  * link counter to determine the number of hard links
  * Pointers to the blocks storing file’s contents
  */
+
+
+/* According to lecutre notes 
+ * I-node contains: owner, type (directory, file, device), last modified
+ * time, last accessed time, last I-node modified time, access
+ * permissions, number of links to the file, size, and block pointers 
+ */
 typedef struct inode{
 	short iType;			// dir or file //AK: Why short, if either dir or file we can just use char short is 2 bytes, we dont need 2^16 optiomns
 	int ino;				// inode number
@@ -37,11 +46,20 @@ typedef struct inode{
 	time_t mtime;			// inode modification time
 	uid_t userID;			// user id
 	gid_t groupID;			// group id
+	// number of links (alias)
+	// direct pointers to the datablocks for the file
+	// does not contain file path
 } inode;
 
 inode *inodeTable;			// Global inode table
 
 // Superblock to describe filesystem
+// Each UNIX partition usually contains a special block called the superblock. 
+// The superblock contains the basic information about the entire file system. 
+// This includes the size of the file system, the list of free and allocated 
+// blocks, the name of the partition, and the modification time of the filesystem.
+
+
 /* Struct
  * s_list: superblock LL
  * s_dev: = Device indentifer
@@ -53,10 +71,46 @@ inode *inodeTable;			// Global inode table
  * s_io = inodes waiting for write
  * s_files = list of file objects
  */
-typdef struct sblock {
-	int num_nodes;
-	int 
 
+// From class notes slide 24
+/* Super Block contains
+ * size of file system, number of free blocks, list of free blocks, 
+ * index to the next free block, size of the I-node list,
+ * number of free I-nodes, list of free I-nodes, index to the next free
+ * I-node, locks for free block and free I-node lists, and flag to indicate
+ * a modification to the SB
+ */
+
+typdef struct sblock {
+	int num_inodes;
+	int fs_size;
+	int block_size;
+	int num_free_blocks;
+	int index_next_free_block;
+	int num_free_inodes;
+	int index_next_free_inode;
+	int free_inode_list;
+	int mod_flag;
+	//list of free and allocated blocks
+    // name of parition
+	time_t atime; // last modified time
+	// magic number?
+	int root_inode_num; // Inode number of root directory
+	// pointer to list of free blocks
+}
+
+// Not sure if be necessary but bitmap to keep track of blocks as in slide 13 of filesystem 
+// slides
+
+/* Bitmap: one bit for each block on the disk
+ * Good to find a contiguous group of free blocks
+ * Files are often accessed sequentially
+ * For 1TB disk and 4KB blocks, 32MB for the bitmap
+ * Chained free portions: pointer to the next one
+ * Not so good for sequential access (hard to find sequential
+ * blocks of appropriate size)
+ * Index: treats free space as a file
+ */
 
 
 #endif
