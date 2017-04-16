402fe1c4471f79dc98b43e10a625c21f06cb8f7c
Andrew Khazanovich <akhaz@assembly.cs.rutgers.edu>
2017-04-16 11:18:18 -0400

    Somes comments about inode structure and added few attributes to inode structure

--- stdefs.h
+++ stdefs.h
@@ -13,18 +13,35 @@
 /****************************************************************/
 // Structure Definitions
 /****************************************************************/
+
+
+/* Inode Basics
+ * An Inode number points to an Inode. An Inode is a data structure that stores the following information about a file :
+ * Size of file
+ * Device ID
+ * User ID of the file
+ * Group ID of the file
+ * The file mode information and access privileges for owner, group and others
+ * File protection flags
+ * The timestamps for file creation, modification etc
+ * link counter to determine the number of hard links
+ * Pointers to the blocks storing file’s contents
+ */
 typedef struct inode{
-	short iType;			// dir or file
-	int ino;			// inode number
-	long int size;			// data block size (bytes) | 0 = free
+	short iType;			// dir or file //AK: Why short, if either dir or file we can just use char short is 2 bytes, we dont need 2^16 optiomns
+	int ino;				// inode number
+	long int size;			// data block size (bytes) | 0 = free | Around 4 GB
 	int fileMode;			// Permissions
 	time_t atime;			// inode access time
 	time_t ctime;			// inode change time
 	time_t mtime;			// inode modification time
+	uid_t userID			// user id
+	gid_t groupID			// group id
 } inode;
 
 inode *inodeTable;			// Global inode table
 char initTracker;			// Make sure init doesn't get called more than once 
+
 
 
 #endif
